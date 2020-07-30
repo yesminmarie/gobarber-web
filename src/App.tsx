08ffe9fbddfde505dc738a929097e386bbddcdf0
yesmin <ymslahoud@gmail.com>
2020-07-30 20:59:57 -0300

    context-api

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,16 @@ import SignIn from './pages/SignIn';
 // import SignUp from './pages/SignUp';
 import GlobalStyle from './styles/global';
 
+import AuthContext from './context/AuthContext';
+
+// <AuthContext.Provider> indica que todo componente dentro dele
+// terá acesso à informação de autenticação
+
 const App: React.FC = () => (
   <>
-    <SignIn />
+    <AuthContext.Provider value={{ name: 'Yesmin' }}>
+      <SignIn />
+    </AuthContext.Provider>
     <GlobalStyle />
   </>
 );
